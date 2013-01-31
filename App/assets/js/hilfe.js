733839a638afb1ccccddb713f9a0a67b71fdde91
Michael iMac <besitzer@new-host.home>
2013-01-31 09:35:19 +0100

    Hilfe iPad überarbeitet dynamische Hintergründe

--- App/assets/js/hilfe.js
+++ App/assets/js/hilfe.js
@@ -1,3 +1,23 @@
+window.addEventListener("load",function(){
+
+if(localStorage.tempJson != undefined){
+
+	console.log("ls da");
+	var obj = JSON.parse(localStorage.tempJson);
+	var kat = obj.kat.id;
+	console.log(kat);
+	document.body.style.backgroundImage = "url(assets/img/spiel/"+kat+"_hintergrund.png)";
+
+}else{
+
+	document.body.style.backgroundImage = "url(assets/img/spiel/0100_hintergrund.png)";
+}
+
+},false);
+
+
+
+
 function showHilfe(){	
 
 	var url = window.location.pathname;
